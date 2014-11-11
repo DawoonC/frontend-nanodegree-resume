3265b4d5240d0673bcbc11a534832a1b50217269
DawoonC <cdw_3@naver.com>
2014-11-11 18:36:55 +0900

    replace jQuery with CDN

--- js/interaction.js
+++ js/interaction.js
@@ -1,16 +1,18 @@
-var navPosition = $('#nav-bar').offset().top -10;
-var navBar = document.getElementById('nav-bar');
-var header = document.getElementById('header');
+$(function() {
+  var navPosition = $('#nav-bar').offset().top -10;
+  var navBar = document.getElementById('nav-bar');
+  var header = document.getElementById('header');
 
-function navControl() {
-  var currentScroll = $(document).scrollTop();
-  if (currentScroll >= navPosition) {
-    navBar.classList.add('fixed-nav');
-    header.classList.add('expand-header');
-  } else {
-    navBar.classList.remove('fixed-nav');
-    header.classList.remove('expand-header');
-  }  
-}
+  function navControl() {
+    var currentScroll = $(document).scrollTop();
+    if (currentScroll >= navPosition) {
+      navBar.classList.add('fixed-nav');
+      header.classList.add('expand-header');
+    } else {
+      navBar.classList.remove('fixed-nav');
+      header.classList.remove('expand-header');
+    }  
+  }
 
-$(window).scroll(navControl);
+  $(window).scroll(navControl);
+});
